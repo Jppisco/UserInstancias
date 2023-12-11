66aa6da95165708989e7f511249fc0028c2ab665
juan <juan123pisco765@gmail.com>
2023-12-10 21:02:06 -0500

    refactor

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -17,79 +17,69 @@ export class UsuarioService {
     private router: Router,
     private errores: ShowErrorService) { }
 
+
   // loginGoogle() {
-  //   return this.authLogin(new GoogleAuthProvider())
+  //   return this.authLogin(new GoogleAuthProvider());
   // }
+
   // async authLogin(provider: any) {
   //   try {
-  //     const result = await this.afAuth.signInWithPopup(provider)
+  //     const result = await this.afAuth.signInWithPopup(provider);
   //     const user = result.user;
-  //     const idToken = await user.getIdToken()
+  //     const idToken = await user.getIdToken();
   //     const userData = {
   //       uid: user.uid,
   //       correo: user.email,
   //       token: idToken
   //     };
-  //     const userDataString = JSON.stringify(userData);
-  //     this.getcorreoBy(user.email).subscribe(data => {
-  //       const correoEnsayo = data[0]?.email;
+  //     // No es necesario convertir userData a cadena JSON antes de almacenarlo en sessionStorage
+  //     sessionStorage.setItem('userData', JSON.stringify(userData));
+  //     this.validateEmail(user.email).subscribe(data => {
 
-  //       if (correoEnsayo !== undefined) {
-  //         console.log(correoEnsayo)
-  //         // Almacena la cadena JSON en sessionStorage
-  //         sessionStorage.setItem('userData', JSON.stringify(userDataString));
-  //         this.router.navigate(['/list-I']);
-  //       } else {
-  //         this.errores.showErrorCenter('Correo inválido');
-  //       }
+  //       if (data.length == 0) {
+  //         sessionStorage.clear()
+  //         return this.errores.showErrorCenter('Correo Invalido')
+  //       };
+  //       console.log(data);
+
+  //       // console.log(data[0].payload.doc.data()['rol'])
+  //       const { rol } = data[0].payload.doc.data()
+  //       console.log(rol)
+  //       sessionStorage.setItem('rol', rol);
+  //       this.router.navigate(['/list-I']);
+  //       return 0;
+
   //     });
   //   } catch (error) {
   //     console.log(error);
   //   }
-
   // }
-  // getcorreoBy(email: string): Observable<any> {
-  //   return this.firestore.collection('userInfo', ref => ref.where('email', '==', email)).valueChanges();
+
+  // validateEmail(email: string): Observable<any> {
+  //   return this.firestore.collection('userInfo', ref => ref.where('email', '==', email)).snapshotChanges();
   // }
   loginGoogle() {
-    return this.authLogin(new GoogleAuthProvider());
+    return this.authLogin(new GoogleAuthProvider())
   }
-
   async authLogin(provider: any) {
     try {
-      const result = await this.afAuth.signInWithPopup(provider);
+      const result = await this.afAuth.signInWithPopup(provider)
       const user = result.user;
-      const idToken = await user.getIdToken();
+      const idToken = await user.getIdToken()
       const userData = {
         uid: user.uid,
         correo: user.email,
         token: idToken
       };
-      // No es necesario convertir userData a cadena JSON antes de almacenarlo en sessionStorage
-      sessionStorage.setItem('userData', JSON.stringify(userData));
-      this.validateEmail(user.email).subscribe(data => {
+      const userDataString = JSON.stringify(userData);
 
-        if (data.length == 0) {
-          sessionStorage.clear()
-          return this.errores.showErrorCenter('Correo Invalido')
-        };
-        console.log(data);
+      // Almacena la cadena JSON en sessionStorage
+      sessionStorage.setItem('userData', userDataString);
+      this.router.navigate(['/list-I']);
 
-        // console.log(data[0].payload.doc.data()['rol'])
-        const { rol } = data[0].payload.doc.data()
-        console.log(rol)
-        sessionStorage.setItem('rol', rol);
-        this.router.navigate(['/list-I']);
-        return 0;
-
-      });
     } catch (error) {
       console.log(error);
     }
-  }
-
-  validateEmail(email: string): Observable<any> {
-    return this.firestore.collection('userInfo', ref => ref.where('email', '==', email)).snapshotChanges();
   }
 
   getGlukerId(email: string): Promise<any> {
